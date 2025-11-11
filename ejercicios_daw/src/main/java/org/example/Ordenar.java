121cf5eb82717659f486fe167c558ae448cc17ea
A600\Patricia <p.benaventedomenec@edu.gva.es>
2025-11-11 21:14:50 +0100

    ++

---
+++ ejercicios_daw/src/main/java/org/example/Ordenar.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Ordenar {
+    static void main() {
+
+        int vector[] = {7,5,10,2,1};
+
+        Arrays.sort(vector);
+        System.out.println(Arrays.toString(vector));
+
+    }
+}
